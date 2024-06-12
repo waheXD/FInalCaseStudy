2405771a61ed62ef479c1ea74133c17230a978d3
Cedrixmonreal14 <monrealcedrix28@gmail.com>
2024-06-13 02:00:03 +0800

    modify patient end

--- sanctum-app/src/components/Patient/Appointment.vue
+++ sanctum-app/src/components/Patient/Appointment.vue
@@ -93,44 +93,7 @@ export default {
     CancelAppointment,
   },
   methods: {
-    async fetchAppointments() {
-      await this.$store.dispatch(
-        "fetchAppointments",
-        localStorage.getItem("user_id")
-      );
-    },
-    toggleAddAppointment() {
-      this.showAppointmentModal = !this.showAppointmentModal;
-
-      if (this.showAppointmentModal == false) {
-        this.$store.dispatch(
-          "fetchAppointments",
-          localStorage.getItem("user_id")
-        );
-      }
-    },
-    toggleAppointment(id) {
-      this.$store.getters.getAppointment(id);
-      this.showViewAppointmentModal = !this.showViewAppointmentModal;
-
-      if (this.showViewAppointmentModal == false) {
-        this.$store.dispatch(
-          "fetchAppointments",
-          localStorage.getItem("user_id")
-        );
-      }
-    },
-    cancelAppointment(id) {
-      this.$store.getters.getAppointment(id);
-      this.showCancelAppointmentModal = !this.showCancelAppointmentModal;
-
-      if (this.showCancelAppointmentModal == false) {
-        this.$store.dispatch(
-          "fetchAppointments",
-          localStorage.getItem("user_id")
-        );
-      }
-    },
+    
   },
   computed: {
     appointments() {
